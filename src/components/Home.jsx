8914049f9917f38c73405bb14de041ca13e02928
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-02-20 13:36:00 +0530

    added button  Past Quiz Attempts over Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 function Home() {
+   const navigate = useNavigate();
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-6">Interactive Quiz Platform</h1>
@@ -9,6 +10,13 @@ function Home() {
           Start Quiz
         </button>
       </Link>
+
+      <button
+        className="mt-6 px-6 py-2 bg-blue-500 hover:bg-blue-700 rounded"
+        onClick={() => navigate("/history")}
+      >
+        Past Quiz Attempts
+      </button>
     </div>
   );
 }
